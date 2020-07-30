3d0043f349230dce4e966bf5e8f8d3e6ce0c1133
Dilant Patel <Dilant.Patel@Logianalytics.com>
2020-07-29 23:24:34 -0400

    initialize directories

---
+++ AddToComposer/customJs.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+		//Observe and fire when Dashboard is initialized and ready.
+	var observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			if (mutation.attributeName === "class") {
+				if ($(mutation.target).hasClass('screenshot_status_ready')) {
+					if(typeof zd != 'undefined' 
+						&& typeof zd.dashboardView != 'undefined' 
+						&& typeof zd.dashboardView._controls != 'undefined') {
+						observer.disconnect();
+						const constantMock = window.fetch;
+						window.fetch = function() {
+							return new Promise((resolve, reject) => {
+								constantMock.apply(this, arguments)
+									.then((response) => {
+										if(response.url.indexOf('/api/actions/') > -1 && response.url.indexOf('/invoke') > -1){
+											var fltr = JSON.parse(arguments[1].body).startVisMessage.filters[0];
+											var actionLabel = fltr.path.name + '-';
+											actionLabel += fltr.value[0];
+											parent.postMessage('action_invoked:' + actionLabel.replace(/\s/g, ''), '*');
+											console.log(fltr);
+										}
+										resolve(response);
+									})
+									.catch((error) => {
+										reject(response);
+									})
+							});
+						}
+					}
+				}
+			}
+		});
+	});
+	observer.observe(document.body, {
+		attributes: true
+	});
+	if(document.getElementById('zoomdata-init-loader') !== null) {
+		observer.observe(document.getElementById('zoomdata-init-loader'), {
+			attributes: true
+		});
+	}
+});
